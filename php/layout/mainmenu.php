56ad76647e1ccb769442d101a7b078f9e45c2a2e
nuno <nunopunkas@gmail.com>
2025-05-28 16:39:48 +0200

    config

--- php/layout/mainmenu.php
+++ php/layout/mainmenu.php
@@ -1,9 +1,9 @@
 <nav class="navbar" aria-label="Main Navigation">
     <ul class="navlist">
-    <li><a href="/index.php" class="button">Homepage</a></li>
+    <li><a href="<?php echo BASE_URL; ?>index.php" class="button">Homepage</a></li>
     <li><a href="#" class="button" id="loginBtn">Login</a></li>
-    <li><a href="/signup.php" class="button">Sign Up</a></li>
-    <li><a href="/catalog.php" class="button">Catalog</a></li>
-    <li><a href="/games.php" class="button">Games</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/signup.php" class="button">Sign Up</a></li>
+    <li><a href="<?php echo BASE_URL; ?>catalog.php" class="button">Catalog</a></li>
+    <li><a href="<?php echo BASE_URL; ?>games.php" class="button">Games</a></li>
     </ul>
 </nav>

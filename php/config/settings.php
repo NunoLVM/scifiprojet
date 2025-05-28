c957ff6e8e41d35f1e755574aff1af671237f56d
nuno <nunopunkas@gmail.com>
2025-05-28 16:39:13 +0200

    fichier config

--- php/config/settings.php
+++ php/config/settings.php
@@ -9,8 +9,11 @@ if (ENV == "local") {
     define ("css_dir", "http://popcorn.local/dist/css/");
     define ("js_dir", "http://popcorn.local/dist/js/");
     define ("images_dir", "http://popcorn.local/images/");
+    define ("BASE_URL", "http://popcorn.local/php/"):
+
 } else {
     define ("css_dir", "https://nunoluis.afecdax.ovh/dist/css/");
     define ("js_dir", "https://nunoluis.afecdax.ovh/dist/js/");
     define ("images_dir", "https://nunoluis.afecdax.ovh/images/");
+    define ("BASE_URL", "https://nunoluis.afecdax.ovh/php/"):
 }
--- php/layout/submenu.php
+++ php/layout/submenu.php
@@ -1,7 +1,7 @@
 <nav class="footer-nav" aria-label="Footer Navigation">
     <ul class="footer-navlist">
-        <li><a href="/about.php">About Us</a></li>
-        <li><a href="/contact.php">Contact</a></li>
-        <li><a href="/legal_notice.php">Legal Notice</a></li>
+        <li><a href="<?php echo BASE_URL; ?>about.php">About Us</a></li>
+        <li><a href="<?php echo BASE_URL; ?>contact.php">Contact</a></li>
+        <li><a href="<?php echo BASE_URL; ?>legal_notice.php">Legal Notice</a></li>
     </ul>
 </nav>
